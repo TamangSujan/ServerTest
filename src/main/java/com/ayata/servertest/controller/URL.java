139d89ad92e64cae5154438e5eb044d58c731572
TamangSujan <moktansujanj5@gmail.com>
2022-11-11 16:20:22 +0545

    Git WebHook Check from Jenkins

--- src/main/java/com/ayata/servertest/controller/URL.java
+++ src/main/java/com/ayata/servertest/controller/URL.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class URL {
     @GetMapping("/")
     public String home(){
-        return "Check Another";
+        return "Check Another Check";
     }
 }
