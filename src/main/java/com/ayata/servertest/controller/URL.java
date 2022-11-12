91a36c75096450a7d0ac94ad440f92e78b8a37bb
TamangSujan <moktansujanj5@gmail.com>
2022-11-12 15:38:47 +0545

    Checking GitHub Jenkins Tomcat Integration

--- src/main/java/com/ayata/servertest/controller/URL.java
+++ src/main/java/com/ayata/servertest/controller/URL.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class URL {
     @GetMapping("/")
     public String home(){
-        return "Check Another Check";
+        return "Git Jenkins Tomcat Integration";
     }
 }
