f90dd9951fe497741c53c9fc5b27dc99bd790680
TamangSujan <moktansujanj5@gmail.com>
2022-11-12 16:02:14 +0545

    Restart Jenkins

--- src/main/java/com/ayata/servertest/controller/URL.java
+++ src/main/java/com/ayata/servertest/controller/URL.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class URL {
     @GetMapping("/")
     public String home(){
-        return "Git Jenkins Tomcat Integration - Checking sudo with password";
+        return "Git Jenkins Tomcat Integration";
     }
 }
